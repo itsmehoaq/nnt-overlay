4d821264372d1874cc63abb7b9f74a17509d9c55
FukutoTojido <lohoangkhoinguyen1812@gmail.com>
2023-08-07 00:54:47 +0700

    Change mappool map to flex instead of using absolute position

--- mappool/index.js
+++ mappool/index.js
@@ -1,33 +1,41 @@
-window.addEventListener('contextmenu', (e) => e.preventDefault());
+window.addEventListener("contextmenu", (e) => e.preventDefault());
 
 // START
-let socket = new ReconnectingWebSocket('ws://127.0.0.1:24050/ws');
+let socket = new ReconnectingWebSocket("ws://127.0.0.1:24050/ws");
 let user = {};
 
 // NOW PLAYING
-let mapContainer = document.getElementById('mapContainer');
-let mapArtist = document.getElementById('mapName');
-let mapInfo = document.getElementById('mapInfo');
-let mapper = document.getElementById('mapper');
-let stars = document.getElementById('stars');
-let stats = document.getElementById('stats');
-let red_button = document.getElementById('redPickButton');
-let blue_button = document.getElementById('bluePickButton');
-let autopick_button = document.getElementById('autoPickButton');
+let mapContainer = document.getElementById("mapContainer");
+let mapArtist = document.getElementById("mapName");
+let mapInfo = document.getElementById("mapInfo");
+let mapper = document.getElementById("mapper");
+let stars = document.getElementById("stars");
+let stats = document.getElementById("stats");
+let red_button = document.getElementById("redPickButton");
+let blue_button = document.getElementById("bluePickButton");
+let autopick_button = document.getElementById("autoPickButton");
 
 const beatmaps = new Set();
-const load_maps = async () => await $.getJSON('../_data/beatmap_data.json');
+const load_maps = async () => await $.getJSON("../_data/beatmap_data.json");
 
-socket.onopen = () => { console.log('Successfully Connected'); };
-socket.onclose = event => { console.log('Socket Closed Connection: ', event); socket.send('Client Closed!'); };
-socket.onerror = error => { console.log('Socket Error: ', error); };
+socket.onopen = () => {
+    console.log("Successfully Connected");
+};
+socket.onclose = (event) => {
+    console.log("Socket Closed Connection: ", event);
+    socket.send("Client Closed!");
+};
+socket.onerror = (error) => {
+    console.log("Socket Error: ", error);
+};
 
 let gameState;
 let hasSetup = false;
 let lastPicked = null;
-let redName = 'Red Team', blueName = 'Blue Team';
+let redName = "Red Team",
+    blueName = "Blue Team";
 let tempMapID = 0;
-let currentPicker = 'Red';
+let currentPicker = "Red";
 let enableAutoPick = false;
 let selectedMaps = [];
 
@@ -41,22 +49,22 @@ class Beatmap {
     generate() {
         let mappoolContainer = document.getElementById(`${this.mods}`);
 
-        this.clicker = document.createElement('div');
+        this.clicker = document.createElement("div");
         this.clicker.id = `${this.layerName}Clicker`;
 
         mappoolContainer.appendChild(this.clicker);
         let clickerObj = document.getElementById(this.clicker.id);
 
-        this.bg = document.createElement('div');
-        this.map = document.createElement('div');
-        this.overlay = document.createElement('div');
-        this.blinkoverlay = document.createElement('div');
-        this.artist = document.createElement('div');
-        this.title = document.createElement('div');
-        this.difficulty = document.createElement('div');
-        this.stats = document.createElement('div');
-        this.modIcon = document.createElement('div');
-        this.pickedStatus = document.createElement('div');
+        this.bg = document.createElement("div");
+        this.map = document.createElement("div");
+        this.overlay = document.createElement("div");
+        this.blinkoverlay = document.createElement("div");
+        this.artist = document.createElement("div");
+        this.title = document.createElement("div");
+        this.difficulty = document.createElement("div");
+        this.stats = document.createElement("div");
+        this.modIcon = document.createElement("div");
+        this.pickedStatus = document.createElement("div");
 
         this.bg.id = this.layerName;
         this.map.id = `${this.layerName}BG`;
@@ -69,31 +77,31 @@ class Beatmap {
         this.modIcon.id = `${this.layerName}ModIcon`;
         this.pickedStatus.id = `${this.layerName}STATUS`;
 
-        this.artist.setAttribute('class', 'mapInfo artist');
-        this.title.setAttribute('class', 'mapInfo title');
-        this.difficulty.setAttribute('class', 'mapInfo diff');
-        this.map.setAttribute('class', 'map');
-        this.pickedStatus.setAttribute('class', 'pickingStatus');
-        this.overlay.setAttribute('class', 'overlay');
-        this.blinkoverlay.setAttribute('class', 'blinkoverlay');
-        this.bg.setAttribute('class', 'statBG');
-        this.modIcon.setAttribute('class', `modIcon icon-${this.mods.toLowerCase()}`);
+        this.artist.setAttribute("class", "mapInfo artist");
+        this.title.setAttribute("class", "mapInfo title");
+        this.difficulty.setAttribute("class", "mapInfo diff");
+        this.map.setAttribute("class", "map");
+        this.pickedStatus.setAttribute("class", "pickingStatus");
+        this.overlay.setAttribute("class", "overlay");
+        this.blinkoverlay.setAttribute("class", "blinkoverlay");
+        this.bg.setAttribute("class", "statBG");
+        this.modIcon.setAttribute("class", `modIcon icon-${this.mods.toLowerCase()}`);
         this.modIcon.innerHTML = `${this.modID}`;
-        this.clicker.setAttribute('class', 'clicker');
+        this.clicker.setAttribute("class", "clicker");
+        clickerObj.appendChild(this.overlay);
         clickerObj.appendChild(this.map);
-        document.getElementById(this.map.id).appendChild(this.overlay);
-        document.getElementById(this.map.id).appendChild(this.blinkoverlay);
+        clickerObj.appendChild(this.blinkoverlay);
         document.getElementById(this.map.id).appendChild(this.artist);
         document.getElementById(this.map.id).appendChild(this.title);
         document.getElementById(this.map.id).appendChild(this.difficulty);
-        clickerObj.appendChild(this.pickedStatus);
-        clickerObj.appendChild(this.bg);
         clickerObj.appendChild(this.modIcon);
+        clickerObj.appendChild(this.pickedStatus);
+        // clickerObj.appendChild(this.bg);
 
-        this.clicker.style.transform = 'translateY(0)';
+        this.clicker.style.transform = "translateY(0)";
     }
     grayedOut() {
-        this.overlay.style.opacity = '1';
+        this.overlay.style.opacity = "1";
     }
 }
 
@@ -103,18 +111,23 @@ socket.onmessage = async (event) => {
     if (!hasSetup) setupBeatmaps();
 
     if (blueName !== data.tourney.manager.teamName.right && data.tourney.manager.teamName.right) {
-        blueName = data.tourney.manager.teamName.right || 'Blue';
+        blueName = data.tourney.manager.teamName.right || "Blue";
     }
     if (redName !== data.tourney.manager.teamName.left && data.tourney.manager.teamName.left) {
-        redName = data.tourney.manager.teamName.left || 'Red';
+        redName = data.tourney.manager.teamName.left || "Red";
     }
 
     if (tempMapID !== data.menu.bm.id && data.menu.bm.id != 0) {
         if (tempMapID == 0) tempMapID = data.menu.bm.id;
         else {
             tempMapID = data.menu.bm.id;
-            let pickedMap = Array.from(beatmaps).find(b => b.beatmapID == tempMapID);
-            if (pickedMap && enableAutoPick && !selectedMaps.includes(tempMapID)) pickMap(Array.from(beatmaps).find(b => b.beatmapID == tempMapID), currentPicker == 'Red' ? redName : blueName, currentPicker);
+            let pickedMap = Array.from(beatmaps).find((b) => b.beatmapID == tempMapID);
+            if (pickedMap && enableAutoPick && !selectedMaps.includes(tempMapID))
+                pickMap(
+                    Array.from(beatmaps).find((b) => b.beatmapID == tempMapID),
+                    currentPicker == "Red" ? redName : blueName,
+                    currentPicker
+                );
         }
     }
 };
@@ -126,8 +139,12 @@ async function setupBeatmaps() {
     try {
         $.ajaxSetup({ cache: false });
         const jsonData = await $.getJSON(`../_data/beatmaps.json`);
-        jsonData.beatmaps.map((beatmap) => { bms.push(beatmap); });
-    } catch (error) { console.error('Could not read JSON file', error); }
+        jsonData.beatmaps.map((beatmap) => {
+            bms.push(beatmap);
+        });
+    } catch (error) {
+        console.error("Could not read JSON file", error);
+    }
 
     let row = -1;
     let preMod = 0;
@@ -141,19 +158,19 @@ async function setupBeatmaps() {
         }
         const bm = new Beatmap(beatmap.mods, beatmap.identifier, beatmap.beatmap_id, `map${index}`);
         bm.generate();
-        bm.clicker.addEventListener('mousedown', () => {
-            bm.clicker.addEventListener('click', event => {
-                if (!event.shiftKey) event.ctrlKey ? banMap(bm, redName, 'Red') : pickMap(bm, redName, 'Red');
+        bm.clicker.addEventListener("mousedown", () => {
+            bm.clicker.addEventListener("click", (event) => {
+                if (!event.shiftKey) event.ctrlKey ? banMap(bm, redName, "Red") : pickMap(bm, redName, "Red");
                 else resetMap(bm);
             });
-            bm.clicker.addEventListener('contextmenu', event => {
-                if (!event.shiftKey) event.ctrlKey ? banMap(bm, blueName, 'Blue') : pickMap(bm, blueName, 'Blue');
+            bm.clicker.addEventListener("contextmenu", (event) => {
+                if (!event.shiftKey) event.ctrlKey ? banMap(bm, blueName, "Blue") : pickMap(bm, blueName, "Blue");
                 else resetMap(bm);
             });
         });
         const stored_beatmaps = await load_maps();
         const mapData = await getDataSet(stored_beatmaps, beatmap.beatmap_id);
-        bm.map.style.backgroundImage = `url('https://assets.ppy.sh/beatmaps/${mapData.beatmapset_id}/covers/cover.jpg')`;
+        bm.clicker.style.backgroundImage = `url('https://assets.ppy.sh/beatmaps/${mapData.beatmapset_id}/covers/cover.jpg')`;
         bm.artist.innerHTML = `${mapData.artist}`;
         bm.title.innerHTML = `${mapData.title}`;
         bm.difficulty.innerHTML = `[${mapData.version}] by ${mapData.creator}`;
@@ -161,95 +178,93 @@ async function setupBeatmaps() {
     });
 }
 
-const getDataSet = (stored_beatmaps, beatmap_id) => stored_beatmaps.find(b => b.beatmap_id == beatmap_id) || null;
+const getDataSet = (stored_beatmaps, beatmap_id) => stored_beatmaps.find((b) => b.beatmap_id == beatmap_id) || null;
 
 const pickMap = (bm, teamName, color) => {
-    if (lastPicked !== null) lastPicked.blinkoverlay.style.animation = 'none';
+    if (lastPicked !== null) lastPicked.blinkoverlay.style.animation = "none";
     lastPicked = bm;
     switchPick(color);
 
     document.cookie = `lastPick=${bm.beatmapID}-${color.toLowerCase()};path=/`;
 
-    bm.pickedStatus.style.color = '#f5f5f5';
-    bm.overlay.style.opacity = '0.85';
-    bm.blinkoverlay.style.animation = 'blinker 1s cubic-bezier(.36,.06,.01,.57) 300ms 8, slowPulse 5000ms ease-in-out 8000ms 18';
-    bm.artist.style.opacity = '0.3';
-    bm.title.style.opacity = '0.3';
-    bm.difficulty.style.opacity = '0.3';
-    bm.modIcon.style.opacity = '0.3';
-    bm.bg.style.opacity = '0';
+    bm.pickedStatus.style.color = "#f5f5f5";
+    // bm.overlay.style.opacity = '0.85';
+    bm.blinkoverlay.style.animation = "blinker 1s cubic-bezier(.36,.06,.01,.57) 300ms 8, slowPulse 5000ms ease-in-out 8000ms 18";
+    // bm.artist.style.opacity = '0.3';
+    // bm.title.style.opacity = '0.3';
+    // bm.difficulty.style.opacity = '0.3';
+    // bm.modIcon.style.opacity = '0.3';
+    // bm.bg.style.opacity = '0';
     selectedMaps.push(bm.beatmapID);
 
-    setTimeout(() => {
+    // setTimeout(() => {
         bm.pickedStatus.style.opacity = 1;
-        bm.pickedStatus.style.outline = bm.mods.includes('TB') ? '3px solid #ffffff' : `3px solid ${color == 'Red' ? '#ff8d8d' : '#93b5ff'}`;
-        bm.pickedStatus.innerHTML = bm.mods.includes('TB') ? 'Tiebreaker' : `<b class="pick${color}">${color} Team</b> pick`;
-    }, 300);
-}
+        bm.pickedStatus.style.outline = bm.mods.includes("TB") ? "5px solid #ffffff" : `5px solid ${color == "Red" ? "#f51651" : "#1e88fa"}`;
+        // bm.pickedStatus.innerHTML = bm.mods.includes('TB') ? 'Tiebreaker' : `<b class="pick${color}">${color} Team</b> pick`;
+    // }, 300);
+};
 
 const banMap = (bm, teamName, color) => {
-    if (bm.mods.includes('TB')) return;
-    bm.pickedStatus.style.color = '#f5f5f5';
-    bm.overlay.style.opacity = '0.95';
-    bm.blinkoverlay.style.animation = 'none';
-    bm.artist.style.opacity = '0.3';
-    bm.title.style.opacity = '0.3';
-    bm.difficulty.style.opacity = '0.3';
-    bm.modIcon.style.opacity = '0.3';
-    bm.bg.style.opacity = '0';
+    if (bm.mods.includes("TB")) return;
+    bm.pickedStatus.style.color = "#f5f5f5";
+    bm.overlay.style.opacity = "0.95";
+    bm.blinkoverlay.style.animation = "none";
+    bm.artist.style.opacity = "0.3";
+    bm.title.style.opacity = "0.3";
+    bm.difficulty.style.opacity = "0.3";
+    bm.modIcon.style.opacity = "0.3";
+    bm.bg.style.opacity = "0";
     selectedMaps.push(bm.beatmapID);
 
-    setTimeout(() => {
+    // setTimeout(() => {
         bm.pickedStatus.style.opacity = 1;
-        bm.pickedStatus.style.outline = 'none';
-        bm.pickedStatus.innerHTML = `<b class="pick${color}">${color} Team</b> ban`;
-    }, 300);
-}
+        bm.pickedStatus.style.outline = "none";
+        bm.pickedStatus.innerHTML = `<b class="pick${color}">BANNED</b>`;
+    // }, 300);
+};
 
-const resetMap = bm => {
+const resetMap = (bm) => {
     document.cookie = `lastPick=;path=/`;
 
-    bm.overlay.style.opacity = '0.5';
-    bm.blinkoverlay.style.animation = 'none';
-    bm.artist.style.opacity = '1';
-    bm.title.style.opacity = '1';
-    bm.difficulty.style.opacity = '1';
-    bm.modIcon.style.opacity = '1';
-    bm.bg.style.opacity = '1';
-    bm.pickedStatus.style.opacity = '0';
-    bm.pickedStatus.style.boxShadow = 'none';
-    bm.pickedStatus.style.outline = 'none';
-    selectedMaps = selectedMaps.filter(e => e !== bm.beatmapID);
+    bm.overlay.style.opacity = "0.5";
+    bm.blinkoverlay.style.animation = "none";
+    bm.artist.style.opacity = "1";
+    bm.title.style.opacity = "1";
+    bm.difficulty.style.opacity = "1";
+    bm.modIcon.style.opacity = "1";
+    bm.bg.style.opacity = "1";
+    bm.pickedStatus.style.opacity = "0";
+    bm.pickedStatus.style.boxShadow = "none";
+    bm.pickedStatus.style.outline = "none";
+    selectedMaps = selectedMaps.filter((e) => e !== bm.beatmapID);
 
     setTimeout(() => {
         bm.pickedStatus.style.opacity = 0;
-        bm.pickedStatus.style.outline = 'none';
-        bm.pickedStatus.innerHTML = '';
+        bm.pickedStatus.style.outline = "none";
+        bm.pickedStatus.innerHTML = "";
     }, 100);
-}
+};
 
-const switchPick = color => {
-    if (!color) currentPicker = currentPicker == 'Red' ? 'Blue' : 'Red';
-    else currentPicker = color == 'Red' ? 'Blue' : 'Red';
-    if (currentPicker == 'Red') {
+const switchPick = (color) => {
+    if (!color) currentPicker = currentPicker == "Red" ? "Blue" : "Red";
+    else currentPicker = color == "Red" ? "Blue" : "Red";
+    if (currentPicker == "Red") {
         red_button.style.opacity = 1;
         blue_button.style.opacity = 0.5;
-    }
-    else {
+    } else {
         red_button.style.opacity = 0.5;
         blue_button.style.opacity = 1;
     }
-}
+};
 
 const switchAutoPick = () => {
     if (enableAutoPick) {
         enableAutoPick = false;
-        autopick_button.innerHTML = 'ENABLE AUTOPICK';
-        autopick_button.style.backgroundColor = '#fc9f9f';
-    }
-    else {
+        autopick_button.innerHTML = "ENABLE AUTOPICK";
+        autopick_button.style.backgroundColor = "#fc9f9f";
+    } else {
         enableAutoPick = true;
-        autopick_button.innerHTML = 'DISABLE AUTOPICK';
-        autopick_button.style.backgroundColor = '#9ffcb3';
+        autopick_button.innerHTML = "DISABLE AUTOPICK";
+        autopick_button.style.backgroundColor = "#9ffcb3";
     }
-}
+};
